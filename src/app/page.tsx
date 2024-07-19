307965b7be415e63393b7aaec490fa443433c75e
zackworld <84471237+gauravzack12@users.noreply.github.com>
2024-07-19 17:46:19 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -130,7 +130,7 @@ export default function Home() {
         <p className="text-gray-600">
           Watch the{' '}
           <a
-            href="https://www.instagram.com/p/C2B1DP0LqBl/"
+            href="https://www.instagram.com/p/C9mopThBLHx/"
             target="_blank"
             className="underline cursor-pointer hover:text-gray-900"
           >
